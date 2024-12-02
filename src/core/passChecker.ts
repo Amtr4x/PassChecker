c5e0ecf94bbd9eb9c66b9a8c73162e9a0f474553
Amtr4x <amtr4x@gmail.com>
2024-12-02 10:28:25 -0500

    ADD rateLength function to passchecker

--- src/core/passChecker.ts
+++ src/core/passChecker.ts
@@ -11,3 +11,24 @@ function checkValidation(password: string): boolean {
     return true;
   }
 }
+
+/**
+ * Check the length of the password and returns a rating
+ * according to security password rules.
+ *
+ * @param password a string representing the user password
+ * @returns a number to represent the password length validity
+ * */
+function rateLength(password: string): number {
+  let rating = 0;
+
+  if (password.length >= 8) {
+    rating += 5;
+  }
+
+  if (password.length >= 12) {
+    rating += 10;
+  }
+
+  return rating;
+}
