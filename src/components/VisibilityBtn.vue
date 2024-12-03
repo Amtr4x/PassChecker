78c611d144d3283295da10f66d61e76090850274
Amtr4x <amtr4x@gmail.com>
2024-12-03 10:28:54 -0500

    ADD VisibilityBtn component

---
+++ src/components/VisibilityBtn.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { IconEye, IconEyeOff } from "@tabler/icons-vue";
+
+const props = defineProps({
+  inputType: String,
+});
+</script>
+
+<template>
+  <button
+    class="p-1 rounded-md shadow-md duration-150 active:shadow-sm hover:scale-105 active:scale-95 shadow-slate-300"
+    :class="props.inputType === 'password' ? 'bg-red-200' : 'bg-green-200'"
+  >
+    <IconEye v-if="props.inputType === 'password'" />
+    <IconEyeOff v-else />
+  </button>
+</template>
