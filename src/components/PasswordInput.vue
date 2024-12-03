4bb326c98858f5700f2bd78670af3ec12c331b5f
Amtr4x <amtr4x@gmail.com>
2024-12-03 15:38:14 -0500

    ADD PasswordInput component

---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import CopyBtn from "./CopyBtn.vue";
+import VisibilityBtn from "./VisibilityBtn.vue";
+
+defineProps<{
+  inputType: string;
+  password: string;
+}>();
+
+defineEmits(["rate-password", "toggle-visibility"]);
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <VisibilityBtn
+      :input-type="inputType"
+      @click="$emit('toggle-visibility')"
+    />
+    <input
+      :value="password"
+      @input="$emit('rate-password', ($event.target as HTMLInputElement).value)"
+      :type="inputType"
+      placeholder="insecure1234..."
+      autofocus
+      autocomplete="off"
+      id="user-passwd"
+      class="p-1 bg-slate-100 rounded-md focus:outline-green-200 shadow-md shadow-slate-200"
+    />
+    <CopyBtn :password="password" />
+  </div>
+</template>
