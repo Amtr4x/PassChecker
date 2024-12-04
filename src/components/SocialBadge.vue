0c58500bae942408cc1523928759bdbdac3ee18d
Amtr4x <amtr4x@gmail.com>
2024-12-03 21:38:29 -0500

    ADD SocialBadge component

---
+++ src/components/SocialBadge.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  username: string;
+  link: string;
+  iconSrc: string;
+  bgColor: string;
+  fgColor: string;
+}>();
+</script>
+
+<template>
+  <a
+    class="font-medium rounded-md p-1 flex gap-1 duration-150 active:scale-95 hover:scale-105"
+    :class="bgColor + ' ' + fgColor"
+    :href="link"
+    target="_blank"
+    noopener
+    noreferrer
+  >
+    {{ username }}
+    <img :src="iconSrc" />
+  </a>
+</template>
